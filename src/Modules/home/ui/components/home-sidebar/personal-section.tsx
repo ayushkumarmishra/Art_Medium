7190f8a1cb306bc3574782853f1f6e962f81d5fe
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-02-16 23:07:37 +0530

    sidebar component completion and logo change

---
+++ src/Modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "History",
+    icon: HistoryIcon,
+    url: "/playlist/history",
+  },
+  {
+    title: "Liked Videos",
+    icon: ThumbsUpIcon,
+    url: "/playlist/liked",
+    auth: true,
+  },
+  {
+    title: "All Playlists",
+    icon: ListVideoIcon,
+    url: "/playlist",
+  },
+];
+
+const PersonalSection = () => {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>You</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false}
+                onClick={() => {}}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
+
+export default PersonalSection;
