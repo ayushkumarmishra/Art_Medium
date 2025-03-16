dc8712ab923017e0df270a089865eb9dc12d1f62
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-03-16 17:44:37 +0530

    Added the carousel and the values on ui

--- src/Modules/home/ui/sections/categories-section.tsx
+++ src/Modules/home/ui/sections/categories-section.tsx
@@ -3,6 +3,7 @@
 import { trpc } from "@/trpc/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
+import { FilterCarousel } from "@/components/filter-carousel";
 
 interface CategoriesSectionProps {
   categoryId?: string;
@@ -11,10 +12,15 @@ interface CategoriesSectionProps {
 const CategorizationSection = ({ categoryId }: CategoriesSectionProps) => {
   const [categories] = trpc.categories.getMany.useSuspenseQuery();
 
+  const data = categories.map((category) => ({
+    value: category.id,
+    label: category.name,
+  }));
+
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <ErrorBoundary fallback={<p>Error...</p>}>
-        <div>{JSON.stringify(categories)}</div>
+        <FilterCarousel value={categoryId} data={data} />
       </ErrorBoundary>
     </Suspense>
   );
---
+++ src/components/filter-carousel.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Badge } from "./ui/badge";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+
+interface FilterCarouselProps {
+  value?: string | null;
+  isLoading?: boolean;
+  onSelect?: (value: string | null) => void;
+  data: {
+    value: string;
+    label: string;
+  }[];
+}
+
+export const FilterCarousel = ({
+  value,
+  onSelect,
+  isLoading,
+  data,
+}: FilterCarouselProps) => {
+  return (
+    <div className="realtive w-full">
+      <Carousel
+        opts={{
+          align: "start",
+          dragFree: true,
+        }}
+        className="w-full px-12"
+      >
+        <CarouselContent className="-ml-3">
+          <CarouselItem className="pl-3 basis-auto">
+            <Badge
+              variant={!value ? "default" : "secondary"}
+              className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+            >
+              All
+            </Badge>
+          </CarouselItem>
+
+          {!isLoading &&
+            data.map((item) => (
+              <CarouselItem key={item.value} className="pl-3 basis-auto">
+                <Badge
+                  variant={value === item.value ? "default" : "secondary"}
+                  className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+                >
+                  {item.label}
+                </Badge>
+              </CarouselItem>
+            ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-0 z-20" />
+        <CarouselNext className="right-0 z-20" />
+      </Carousel>
+    </div>
+  );
+};
