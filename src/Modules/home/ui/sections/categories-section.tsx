0c1f37c0177245736a7e449ffb149b803bfd2030
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-03-16 20:19:45 +0530

    Added the onselect functionality for each categories and set the urls accordingly.

--- src/Modules/home/ui/sections/categories-section.tsx
+++ src/Modules/home/ui/sections/categories-section.tsx
@@ -4,12 +4,14 @@ import { trpc } from "@/trpc/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 import { FilterCarousel } from "@/components/filter-carousel";
+import { useRouter } from "next/navigation";
 
 interface CategoriesSectionProps {
   categoryId?: string;
 }
 
 const CategorizationSection = ({ categoryId }: CategoriesSectionProps) => {
+  const router = useRouter();
   const [categories] = trpc.categories.getMany.useSuspenseQuery();
 
   const data = categories.map((category) => ({
@@ -17,16 +19,24 @@ const CategorizationSection = ({ categoryId }: CategoriesSectionProps) => {
     label: category.name,
   }));
 
+  const onSelect = (value: string | null) => {
+    const url = new URL(window.location.href);
+
+    if (value) {
+      url.searchParams.set("categoryId", value);
+    } else {
+      url.searchParams.delete("categoryId");
+    }
+
+    router.push(url.toString());
+  };
+
   return (
     <Suspense
       fallback={<FilterCarousel isLoading data={[]} onSelect={() => {}} />}
     >
       <ErrorBoundary fallback={<p>Error...</p>}>
-        <FilterCarousel
-          onSelect={(x) => console.log(x)}
-          value={categoryId}
-          data={data}
-        />
+        <FilterCarousel onSelect={onSelect} value={categoryId} data={data} />
       </ErrorBoundary>
     </Suspense>
   );
