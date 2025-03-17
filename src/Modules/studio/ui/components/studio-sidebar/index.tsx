58e8943c5aa346c0f6f92c9adcab819d840ebecb
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-03-17 16:28:41 +0530

    Created studio sidebar and avatar component

--- src/Modules/studio/ui/components/studio-sidebar/index.tsx
+++ src/Modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,16 +1,55 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+"use client";
 
-import MainSection from "./main-section";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import { Separator } from "@/components/ui/separator";
-import PersonalSection from "./personal-section";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import StudioSidebarHeader from "./studio-sidebar-header";
 
 const StudioSidebar = () => {
+  const pathname = usePathname();
+
   return (
     <Sidebar className="pt-16 z-40" collapsible="icon">
       <SidebarContent className="bg-background">
-        <MainSection />
-        <Separator />
-        <PersonalSection />
+        <SidebarGroup>
+          <SidebarMenu>
+            <StudioSidebarHeader />
+
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                isActive={pathname === "/studio"}
+                tooltip="Exit studio"
+                asChild
+              >
+                <Link href="/studio">
+                  <VideoIcon className="size-5" />
+                  <span className="text-sm">Content</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <Separator />
+
+            <SidebarMenuItem>
+              <SidebarMenuButton tooltip="Exit studio" asChild>
+                <Link href="/">
+                  <LogOutIcon className="size-5" />
+                  <span className="text-sm">Exit Studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   );
---
+++ src/Modules/studio/ui/components/studio-sidebar/studio-sidebar-header.tsx
@@ -0,0 +1,29 @@
+import { SidebarHeader } from "@/components/ui/sidebar";
+import { UserAvatar } from "@/components/user-avatar";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const StudioSidebarHeader = () => {
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (
+    <SidebarHeader className="flex items-center justify-center pb-4">
+      <Link href="/users/current">
+        <UserAvatar
+          imageUrl={user.imageUrl}
+          name={user.fullName ?? "User"}
+          className="size-[112px] hover:opacity-80 transition-opacity"
+        />
+      </Link>
+      <div className="flex flex-col items-center mt-2">
+        <p className="text-sm font-medium">Your Profile</p>
+        <p className="text-xs text-muted-foreground ">{user.fullName}</p>
+      </div>
+    </SidebarHeader>
+  );
+};
+
+export default StudioSidebarHeader;
