54c68a98de74830782723391e24aeb26e43b5364
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-03-19 01:11:35 +0530

    created the ui for video section

--- src/Modules/studio/ui/sections/videos-section.tsx
+++ src/Modules/studio/ui/sections/videos-section.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 import { trpc } from "@/trpc/client";
+import Link from "next/link";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -16,7 +26,7 @@ export const VideosSection = () => {
 };
 
 export const VideosSectionSuspense = () => {
-  const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: 5,
     },
@@ -27,7 +37,43 @@ export const VideosSectionSuspense = () => {
 
   return (
     <div>
-      Video Section : {JSON.stringify(data)}
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Views</TableHead>
+              <TableHead>Comments</TableHead>
+              <TableHead>Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>Visibility</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>date</TableCell>
+                    <TableCell>views</TableCell>
+                    <TableCell>comments</TableCell>
+                    <TableCell>likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+
       <InfiniteScroll
         isManual
         hasNextPage={query.hasNextPage}
