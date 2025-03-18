5ef90d81dda44e245bcf131845497ddec630fc47
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-03-18 23:47:35 +0530

    created the videos procedure rendering on create button click

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from "../init";
 import { studioRouter } from "@/Modules/studio/server/procedures";
 import { categoriesRouter } from "@/Modules/categories/server/procedures";
+import { videosRouter } from "@/Modules/videos/server/procedures";
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
   studio: studioRouter,
+  videos: videosRouter,
 });
 
 export type AppRouter = typeof appRouter;
