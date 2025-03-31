820e16507315cf8fae0c7d6a84995a823c3d57d2
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-03-31 23:02:57 +0530

    added the change thumbnail functionality insie the thumnail dropdown option inside form detail section

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -1,11 +1,20 @@
-import { StudioLayout } from "@/Modules/studio/ui/layouts/studio-layout";
 import React from "react";
+
+import { StudioLayout } from "@/Modules/studio/ui/layouts/studio-layout";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 const Layout = ({ children }: LayoutProps) => {
-  return <StudioLayout>{children}</StudioLayout>;
+  return (
+    <StudioLayout>
+      <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
+      {children}
+    </StudioLayout>
+  );
 };
 
 export default Layout;
