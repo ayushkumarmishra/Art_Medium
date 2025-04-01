387e60f0e836bf7c90e22136e6d2bb1ae21ae934
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-04-02 01:24:41 +0530

    refactored form section to use the thumbnail and preview that will be directly stored only at uploading

--- src/Modules/videos/server/procedures.ts
+++ src/Modules/videos/server/procedures.ts
@@ -4,6 +4,7 @@ import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { and, eq } from "drizzle-orm";
+import { UTApi } from "uploadthing/server";
 import { z } from "zod";
 
 export const videosRouter = createTRPCRouter({
@@ -119,17 +120,41 @@ export const videosRouter = createTRPCRouter({
         });
       }
 
+      if (existingVideo.thumbnailKey) {
+        const utapi = new UTApi();
+
+        await utapi.deleteFiles(existingVideo.thumbnailKey);
+        await db
+          .update(videos)
+          .set({
+            thumbnailKey: null,
+            thumbnailUrl: null,
+          })
+          .where(and(eq(videos.id, input.id), eq(videos.userId, userId)));
+      }
+
       if (!existingVideo.muxPlaybackId) {
         throw new TRPCError({
           code: "NOT_FOUND",
         });
       }
 
-      const thumbnailUrl = `https://image.mux.com/${existingVideo.muxPlaybackId}/thumbnail.png`;
+      const tempThumbnailUrl = `https://image.mux.com/${existingVideo.muxPlaybackId}/thumbnail.png`;
+      const utapi = new UTApi();
+
+      const uploadedUrl = await utapi.uploadFilesFromUrl(tempThumbnailUrl);
+
+      if (!uploadedUrl.data) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+        });
+      }
+
+      const { key: thumbnailKey, url: thumbnailUrl } = uploadedUrl.data;
 
       const [updatedVideo] = await db
         .update(videos)
-        .set({ thumbnailUrl })
+        .set({ thumbnailUrl, thumbnailKey })
         .where(and(eq(videos.id, input.id), eq(videos.userId, userId)))
         .returning();
 
