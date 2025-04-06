c91d4b626859c0e2172036eba69340a94aacd115
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-04-06 16:44:49 +0530

    Added the title and description icons to generate the content using ai models

--- src/Modules/videos/server/procedures.ts
+++ src/Modules/videos/server/procedures.ts
@@ -178,4 +178,38 @@ export const videosRouter = createTRPCRouter({
 
       return workflowRunId;
     }),
+
+  generateTitle: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+
+      const { workflowRunId } = await workflow.trigger({
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
+        body: { userId, videoId: input.id },
+      });
+
+      return workflowRunId;
+    }),
+
+  generateDescription: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+
+      const { workflowRunId } = await workflow.trigger({
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/description`,
+        body: { userId, videoId: input.id },
+      });
+
+      return workflowRunId;
+    }),
 });
