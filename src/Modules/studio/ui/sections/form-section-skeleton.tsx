9a77e2262a89ce21417292789d834898ec014d2f
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2025-04-06 17:21:09 +0530

    Added the skeleton for form-section page.

--- src/Modules/studio/ui/sections/form-section-skeleton.tsx
+++ src/Modules/studio/ui/sections/form-section-skeleton.tsx
@@ -1,3 +1,68 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const FormSectionSkeleton = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="animate-pulse space-y-6">
+      {/* Header */}
+      <div className="flex items-center justify-between mb-6">
+        <div className="space-y-2">
+          <Skeleton className="h-6 w-40" />
+          <Skeleton className="h-4 w-60" />
+        </div>
+        <div className="flex items-center gap-x-2">
+          <Skeleton className="h-10 w-20 rounded-md" />
+          <Skeleton className="h-10 w-10 rounded-full" />
+        </div>
+      </div>
+
+      {/* Form Body */}
+      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+        {/* Left Section */}
+        <div className="space-y-8 lg:col-span-3">
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-20" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-28" />
+            <Skeleton className="h-40 w-full" />
+          </div>
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-24" />
+            <Skeleton className="h-[84px] w-[153px]" />
+          </div>
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-24" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className="flex flex-col gap-y-8 lg:col-span-2">
+          <div className="flex flex-col gap-4 bg-[#F9F9F9] rounded-xl overflow-hidden h-fit">
+            <Skeleton className="aspect-video w-full" />
+            <div className="p-4 space-y-6">
+              <div className="space-y-1">
+                <Skeleton className="h-3 w-24" />
+                <Skeleton className="h-4 w-40" />
+              </div>
+              <div className="space-y-1">
+                <Skeleton className="h-3 w-28" />
+                <Skeleton className="h-4 w-24" />
+              </div>
+              <div className="space-y-1">
+                <Skeleton className="h-3 w-28" />
+                <Skeleton className="h-4 w-24" />
+              </div>
+            </div>
+          </div>
+
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-24" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
